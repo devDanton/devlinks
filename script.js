af873f1c7b86a260f14421121e34aea6be7cc5f3
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2023-03-02 14:41:20 -0300

    Correction of folder structure

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleMode() {
   const image = document.querySelector('#profile img')
 
   if (html.classList.contains('light')) {
-    image.setAttribute('src', './assets/Avatar-Light.png')
+    image.setAttribute('src', './src/assets/Avatar-Light.png')
     image.setAttribute('alt', 'Light profile image')
   } else {
-    image.setAttribute('src', './assets/Avatar-Dark.png')
+    image.setAttribute('src', './src/assets/Avatar-Dark.png')
     image.setAttribute('alt', 'Dark profile image')
   }
 }
